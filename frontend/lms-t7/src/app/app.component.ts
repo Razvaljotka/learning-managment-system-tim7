12a84db72273f1edfb01c4f1804065376b79b570
Razvaljotka <bokysteam@gmail.com>
2025-07-06 21:06:22 +0200

    master

--- frontend/lms-t7/src/app/app.component.ts
+++ frontend/lms-t7/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'learning-managment-system-tim7';
+  title = 'lms-t7';
 }
